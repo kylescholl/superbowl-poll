e1d50241bfd87ec2064b997aea0f1683919444bf
mccoyLBI <kyle.b.scholl@gmail.com>
2016-02-04 08:20:45 -0500

    - Created project

--- Superbowl Poll/ViewController.swift
+++ Superbowl Poll/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet var broncos:   UIButton!
+	@IBOutlet var panthers:  UIButton!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
 }
 
